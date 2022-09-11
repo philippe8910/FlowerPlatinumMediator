aba913f1b262e0cd82a4185c2ad174553276fa50
UsagihimeDenka <philippe8910@gmail.com>
2022-09-12 05:50:46 +0800

    first presentation

--- Assets/ChangeScenesSystem.cs
+++ Assets/ChangeScenesSystem.cs
@@ -4,14 +4,19 @@ using System.Collections.Generic;
 using Events;
 using Project;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Valve.VR.InteractionSystem;
 
 public class ChangeScenesSystem : MonoBehaviour
 {
     [SerializeField] private string levelName;
+
+    public GameObject player;
     
     private void OnChangeScenes()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+        Destroy(player);
         EventBus.CleanUpAllData();
     }
 
