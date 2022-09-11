b0c82fc8b1c4d199e197adde04b87d47faf778a5
UsagihimeDenka <philippe8910@gmail.com>
2022-09-12 04:17:57 +0800

    added level_5

--- Assets/TriggerWall.cs
+++ Assets/TriggerWall.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class TriggerWall : MonoBehaviour , ITrigger
 {
     [SerializeField] private Vector3 defaultPos, targetPos;
+
+    public bool isEnter = false;
+
+    private float timer;
     
     public void OnTriggerEnter()
     {
@@ -14,17 +18,31 @@ public class TriggerWall : MonoBehaviour , ITrigger
 
     public void OnTriggerStayTrue()
     {
-        transform.position = Vector3.Lerp(transform.position , targetPos , 0.1f);
+        transform.position = Vector3.Lerp(transform.position , targetPos , 0.5f);
+        timer = 0;
     }
 
     public void OnTriggerStayFalse()
     {
-        transform.position = Vector3.Lerp(transform.position , defaultPos , 0.1f);
+        if (isEnter)
+        {
+            transform.position = Vector3.Lerp(transform.position , defaultPos , 0.5f);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 2)
+            {
+                isEnter = true;
+            }
+        }
 
     }
 
     public void OnTriggerExit()
     {
-        
+        isEnter = false;
+        timer = 0;
     }
 }
